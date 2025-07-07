80840b2ef65e626add11431d93f587929080729f
rneher-igel <neher@igel.com>
2025-07-07 16:33:28 -0600

    mobicontrol initial recipe mobicontrol initial recipe

---
+++ APP_Source/Apps/mobicontrol/README.md
@@ -0,0 +1,18 @@
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## MobiControl Agent for Linux
+
+https://pulse.soti.net/support/soti-mobicontrol/agent-downloads/linux/generic-agent/
+
+```
+Processor: x64
+Device with User Interface: Yes
+Agent Version: Select Latest
+```
+
+LinuxAgent202511_x64_64bit.tar.gz
+
+**NOTE:**
+
+- Obtain the installer, rename to `LinuxAgent_x64_64bit.tar.gz`
+- Obtain the MCSetup.ini file, zip into `MSSetup.zip`
---
+++ APP_Source/Apps/mobicontrol/igel/pre_package_commands.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mkdir -p "%root%/etc/mobicontrol"
+cat <<"EOF" > "%root%/etc/mobicontrol/mobicontrol-init.sh"
+#!/bin/bash
+#set -x
+#trap read debug
+
+ACTION="mobicontrol_${1}"
+
+# App Path
+#APP_PATH="/services_rw/mobicontrol/usr/opt/MobiControl"
+APP_PATH="/wfs/mobicontrol/usr/opt/MobiControl"
+APP_OPT_LINK="/usr/opt/MobiControl"
+
+LOGGER="logger -it ${ACTION}"
+
+if [ ! -d ${APP_PATH} ]; then
+  mkdir -p ${APP_PATH}
+fi
+
+mkdir -p /usr/opt
+ln -svf ${APP_PATH} ${APP_OPT_LINK} | $LOGGER
+
+# Run install.sh for the first time
+if [ ! -e ${APP_PATH}/mobicontrol ]; then
+  cp /services/mobicontrol/opt/mobicontrol/MCSetup.ini /services/mobicontrol/opt/mobicontrol/installer
+  cd /services/mobicontrol/opt/mobicontrol/installer
+  /bin/sh install.sh -c -y | $LOGGER
+fi
+
+# copy mobicontrol to /tmp
+#cp /usr/opt/MobiControl/mobicontrol /tmp
+
+echo "Finished" | $LOGGER
+
+EOF
