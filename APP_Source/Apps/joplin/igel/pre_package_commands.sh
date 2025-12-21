154438d666039aa12e704461141f17328db8ed6d
rneher-igel <neher@igel.com>
2025-12-21 11:24:37 -0700

    joplin initial recipe joplin initial recipe

---
+++ APP_Source/Apps/joplin/igel/pre_package_commands.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p "%root%/etc/joplin"
+cat <<"EOF" > "%root%/etc/joplin/joplin-init.sh"
+#!/bin/bash
+#set -x
+#trap read debug
+
+ACTION="joplin_${1}"
+
+# App Path
+APP_PATH="/services/joplin"
+APP_NAME=$(ls ${APP_PATH}/Joplin-*.AppImage)
+
+LOGGER="logger -it ${ACTION}"
+
+echo "Starting" | $LOGGER
+
+ln -svf ${APP_NAME} /usr/bin/joplin | $LOGGER
+
+echo "Finished" | $LOGGER
+
+EOF
